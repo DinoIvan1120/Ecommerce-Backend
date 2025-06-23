93268904bf818661b910e77bcba456b355e9e1f5
DinoIvan1120 <dinoivan11201994@gmail.com>
2025-06-23 14:56:57 -0500

    Clase BootController

---
+++ src/main/java/com/todotic/bookstoreapi/controller/BootController.java
@@ -0,0 +1,9 @@
+package com.todotic.bookstoreapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/books")
+@RestController
+public class BootController {
+}
